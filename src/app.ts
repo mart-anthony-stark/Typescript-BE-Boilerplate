aa3c71cc9c96338c28484c8c62366fe6c78e14b7
martsalazar <martsalazar0@gmail.com>
2025-08-24 10:29:29 +0800

    feat: add middleware decorator

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
 import { modules } from "@modules/main";
+import config from "config";
 import express from "express";
 
 export function createApp() {
   const app = express();
 
-  app.use(express.json());
+  // Register global middlewares
+  for (const middleware of config.middlewares) {
+    app.use(middleware);
+  }
 
   // Register modules (each is a Router with prefix from @Controller)
   for (const module of modules) {
